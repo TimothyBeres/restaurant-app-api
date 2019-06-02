016cb84fbb1eee19d2b0b3eb1ead4be33faaad49
reiko <reiko.magi@gmail.com>
2019-06-02 13:28:06 +0300

    Good2Have CategoryModel

--- src/main/java/restaurant/model/CategoryItem.java
+++ src/main/java/restaurant/model/CategoryItem.java
@@ -1,4 +1,21 @@
 package restaurant.model;
 
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
 public class CategoryItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String categoryName;
+    @ManyToMany
+    private List<FoodItem> foodItems;
+
+    public CategoryItem(String categoryName, List<FoodItem> foodItems) {
+        this.categoryName = categoryName;
+        this.foodItems = foodItems;
+    }
 }
