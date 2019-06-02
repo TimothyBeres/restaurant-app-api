25f85822a7a29d716d49da60877e1d8f3c304d1f
reiko <reiko.magi@gmail.com>
2019-06-02 12:36:56 +0300

    LowKey Commit!!

--- src/main/java/restaurant/controller/MenuController.java
+++ src/main/java/restaurant/controller/MenuController.java
@@ -3,9 +3,7 @@ package restaurant.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import restaurant.Application;
 import restaurant.model.FoodItem;
 import restaurant.service.MenuService;
@@ -26,4 +24,21 @@ public class MenuController  {
         return menuService.getAll(null,null);
     }
     private static Logger logger = LoggerFactory.getLogger(Application.class);
+
+    //Eelmisel andis array selle id, aga idk mis nyyd id asemel tleb ss :O
+    @GetMapping("{id}")
+    public FoodItem findOne(@PathVariable Long id) {
+        return menuService.findOne(id);
+    }
+
+    @PutMapping("{id}")
+    public FoodItem update(@RequestBody FoodItem menu, @PathVariable Long id) {
+        return menuService.update(menu, id);
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable Long id) {
+        menuService.delete(id);
+    }
+
 }
